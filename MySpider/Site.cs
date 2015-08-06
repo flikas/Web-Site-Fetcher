2df5929542e79d1dea00dbf6cfd67fa806696eb5
flikas <flikas@hotmail.com>
2015-08-06 09:14:20 +0800

    Site & Task Class

---
+++ MySpider/Site.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MySpider
+{
+    class Page
+    {
+        internal Page()
+        {
+            LinkedPages = new List<Page>();
+        }
+        public List<Page> LinkedPages { get; private set; }
+        public Uri Url { get; set; }
+    }
+    class Site : Page
+    {
+        public Site(Uri rootPage) : base() { }
+
+    }
+}
---
+++ MySpider/Task.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MySpider
+{
+    class Task
+    {
+        public Task(Uri destUri, Uri requestUri)
+        {
+            this.DestUri = destUri;
+            this.RequestUri = requestUri;
+            if (DestUri.IsBaseOf(RequestUri))
+                Relativity = 1;
+            else
+                Relativity = 0;
+        }
+        
+        public Uri DestUri { get; private set; }
+        public Uri RequestUri { get; private set; }
+        public int Relativity { get; private set; }
+    }
+}
