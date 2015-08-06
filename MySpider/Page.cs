de5d63bfb68713235b5c7ff92457db4da94b49b9
flikas <flikas@hotmail.com>
2015-08-07 00:42:30 +0800

    Page&Resource OK

---
+++ MySpider/Page.cs
@@ -0,0 +1,104 @@
+﻿using HtmlAgilityPack;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Xml.XPath;
+
+namespace MySpider
+{
+    enum ResourceType { Page, Resource }
+
+    class Resource
+    {
+        public Resource(Uri resUri)
+        {
+            this.Url = resUri;
+            this.Content = null;
+            this.Loaded = false;
+        }
+        public Uri Url { get; protected set; }
+        public byte[] Content { get; protected set; }
+        public bool Loaded { get; protected set; }
+        public ResourceType Type { get; protected set; }
+
+        public void Load(byte[] content, string contentType)
+        {
+            if (this.Content == null)
+            {
+                this.Content = content;
+                if (contentType.Contains("html"))
+                {
+                    this.Type = ResourceType.Page;
+                }
+                else
+                {
+                    this.Type = ResourceType.Resource;
+                }
+                this.Loaded = true;
+            }
+            else
+            {
+                throw new InvalidOperationException();
+            }
+        }
+    }
+
+    class Page : Resource
+    {
+        Page(Resource r)
+            : base(r.Url)
+        {
+            if (!r.Loaded)
+            {
+                throw new InvalidOperationException();
+            }
+            this.Resources = new List<Resource>();
+            MemoryStream ms = new MemoryStream(r.Content);
+            StreamReader rd = new StreamReader(ms);
+            this.HtmlContent = rd.ReadToEnd();
+            Analyse();
+        }
+
+        public string HtmlContent { get; protected set; }
+        public List<Resource> Resources { get; protected set; }
+
+        protected void Analyse()
+        {
+            HtmlDocument hd = new HtmlDocument();
+            hd.LoadHtml(this.HtmlContent);
+            XPathNavigator xn = hd.CreateNavigator();
+
+            while (xn.MoveToFollowing(XPathNodeType.Element))
+            {
+                if (xn.LocalName == "a")
+                {
+                    XPathNavigator xn_attr = xn.Clone();
+                    if (xn_attr.MoveToAttribute("href", xn.NamespaceURI))
+                    {
+                        System.Diagnostics.Debug.Print("GotLink:{0}", xn_attr.Value);
+                        this.Resources.Add(new Resource(new Uri(this.Url, xn_attr.Value)));
+                    }
+                }
+            }
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (obj is Page)
+            {
+                return false;
+            }
+            else
+            {
+                return ((Page)obj).Url.Equals(this.Url);
+            }
+        }
+
+        public override int GetHashCode()
+        {
+            return this.Url.GetHashCode();
+        }
+    }
+}
--- MySpider/Spider.cs
+++ MySpider/Spider.cs
@@ -15,30 +15,16 @@ namespace MySpider
     {
         public Spider() { }
 
-        public void FetchPage(string uri)
+        public void FetchPage(Resource res)
         {
-            WebRequest req = HttpWebRequest.Create(uri);
-            WebResponse resp = req.GetResponse();
-            Stream s = resp.GetResponseStream();
-            StreamReader tr = new StreamReader(s);
-            string buf = tr.ReadToEnd();
-            System.Diagnostics.Debug.Print("Content:{0}", buf);
-            
-            HtmlDocument hd = new HtmlDocument();
-            hd.LoadHtml(buf);
-            XPathNavigator xn = hd.CreateNavigator();
-            //XPathNavigator xn = xd.CreateNavigator();
-            while (xn.MoveToFollowing(XPathNodeType.Element))
+            WebRequest req = HttpWebRequest.Create(res.Url);
+            using (WebResponse resp = req.GetResponse())
             {
-                if (xn.LocalName == "a")
-                {
-                    XPathNavigator xn2 = xn.Clone();
-                    if(xn.MoveToAttribute("href", xn.NamespaceURI))
-                    {
-                        System.Diagnostics.Debug.Print("GotLink:{0}", xn.Value);
-                        xn.MoveTo(xn2);
-                    }
-                }
+                Stream s = resp.GetResponseStream();
+                BinaryReader tr = new BinaryReader(s);
+                byte[] buf = new byte[s.Length];
+                tr.Read(buf, 0, (int)s.Length);
+                res.Load(buf, resp.ContentType);
             }
         }
     }
